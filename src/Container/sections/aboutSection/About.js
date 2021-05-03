66a493230d7ff1eaf0253cdff3f5b698f66828eb
yasserBelatreche <yasserbelatreche000@gmail.com>
2021-05-03 15:04:09 +0100

    about section done

--- src/Container/sections/aboutSection/About.js
+++ src/Container/sections/aboutSection/About.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './about.css'
 import image from '../../../images/marcos-paulo-prado-gKqIvUBsxEc-unsplash.jpg'
+import Dots from './Dots'
 
 const About = () => {
   return (
@@ -11,15 +12,22 @@ const About = () => {
           <div className="underline"></div>
         </div>
 
-        <div className="img-container">
+        <div className="img-container relative">
           <div className="img">
             <img src={image} alt=""/>
           </div>
+
+          <Dots/>
         </div>
 
         <div className="desc">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias magni consequatur vitae sequi nam atque. Odio voluptatem ab eveniet? Harum praesentium minima esse fugiat numquam sint itaque rerum. Iste, itaque.</p>
           <a className="btn" href="#resume">Learn More</a>
+        </div>
+
+        <div className="shapes">
+          <div className="circle"></div>
+          <div className="rectangle"></div>
         </div>
       </div>
     </div>
---
+++ src/Container/sections/aboutSection/Dots.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Dots = () => {
+  return (
+    <div className="dots">
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+      <div className="dot"></div>
+    </div>
+  )
+}
+
+export default Dots;
